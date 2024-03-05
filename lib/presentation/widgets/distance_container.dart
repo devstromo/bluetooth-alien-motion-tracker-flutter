d0b3b19fc2bf69f93885c2f81506458d50b3d7de
alemoles <alemolerio@gmail.com>
2024-03-04 21:56:50 -0300

    create distance container painter

---
+++ lib/presentation/widgets/distance_container.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DistanceContainerPainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+  }
+
+  @override
+  bool shouldRepaint(DistanceContainerPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(DistanceContainerPainter oldDelegate) => false;
+}
