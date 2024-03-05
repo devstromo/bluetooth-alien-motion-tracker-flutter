716d00495a5180113a70ea0be8d65d3a80db4641
alemoles <alemolerio@gmail.com>
2024-03-04 22:00:30 -0300

    add distance data container

---
+++ lib/presentation/widgets/distance_data_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DistanceDataContainer extends StatelessWidget {
+  const DistanceDataContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
