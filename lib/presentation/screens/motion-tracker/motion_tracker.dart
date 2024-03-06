126a12f118d35238f8b8aee9e177cc6e923eceb8
alemoles <alemolerio@gmail.com>
2024-03-06 20:36:57 -0300

    add placeholder motion data

--- lib/presentation/screens/motion-tracker/motion_tracker.dart
+++ lib/presentation/screens/motion-tracker/motion_tracker.dart
@@ -1,5 +1,6 @@
 import 'package:bluetooth_alien_motion_tracker/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class MotionTracker extends StatefulWidget {
   const MotionTracker({super.key});
@@ -9,23 +10,72 @@ class MotionTracker extends StatefulWidget {
 }
 
 class _MotionTrackerState extends State<MotionTracker> {
+  GyroscopeEvent? _gyroscopeEvent;
+  int? _gyroscopeLastInterval;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
       body: Stack(
         children: [
           Center(
-            child: Text(
-              'Motion Tracker',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
+            child: Column(
+              children: [
+                const Text(
+                  'Motion Tracker',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    'Gyroscope',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  _gyroscopeEvent?.x.toStringAsFixed(1) ?? 'x?',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  _gyroscopeEvent?.y.toStringAsFixed(1) ?? 'y?',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  _gyroscopeEvent?.z.toStringAsFixed(1) ?? 'z?',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '${_gyroscopeLastInterval?.toString() ?? '?'} ms',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
           ),
-          Positioned(
+          const Positioned(
             bottom: 0,
             left: 0,
             right: 0,
