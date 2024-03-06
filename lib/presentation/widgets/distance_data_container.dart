7edacbaf783e97534d4467c6e17e3bc9667f4fc0
alemoles <alemolerio@gmail.com>
2024-03-05 22:19:47 -0300

    add fems info data

--- lib/presentation/widgets/distance_data_container.dart
+++ lib/presentation/widgets/distance_data_container.dart
@@ -12,15 +12,28 @@ class DistanceDataContainer extends StatelessWidget {
       color: kDistanceContainerColor,
       child: Stack(
         children: [
+          const Positioned(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'F.E.M.S. 5.327.25',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ),
           Positioned(
             top: 10,
-            left: 0,
+            left: 100,
             right: 0,
             child: Container(
-              color: Colors.red,
-              child: Text('Motion Tracker'),
+              color: Colors.transparent,
+              child: const Text('Motion Tracker'),
             ),
-          )
+          ),
         ],
       ),
     );
