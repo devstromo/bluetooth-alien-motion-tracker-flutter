f7b83fcf491115caff5db5068d70da882e3847ac
alemoles <alemolerio@gmail.com>
2024-03-11 22:31:29 -0300

    create point marker widget

---
+++ lib/presentation/widgets/point_marker.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PointMarker extends StatelessWidget {
+  const PointMarker({
+    super.key,
+    required this.x,
+    required this.y,
+  });
+
+  final double x;
+  final double y;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
