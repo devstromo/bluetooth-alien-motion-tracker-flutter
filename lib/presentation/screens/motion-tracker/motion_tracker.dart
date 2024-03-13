a3e89e723ac6944b72f38e74a8b7e6ee3e418004
alemoles <alemolerio@gmail.com>
2024-03-12 21:51:49 -0300

    code refactor

--- lib/presentation/screens/motion-tracker/motion_tracker.dart
+++ lib/presentation/screens/motion-tracker/motion_tracker.dart
@@ -200,59 +200,6 @@ class _MotionTrackerState extends State<MotionTracker> {
     }
   }
 
-  static TextStyle _computeTextStyle(int rssi) {
-    if (rssi >= -35) {
-      return TextStyle(
-        color: Colors.greenAccent[700],
-      );
-    } else if (rssi >= -45) {
-      return TextStyle(
-        color: Color.lerp(
-          Colors.greenAccent[700],
-          Colors.lightGreen,
-          -(rssi + 35) / 10,
-        ),
-      );
-    } else if (rssi >= -55) {
-      return TextStyle(
-        color: Color.lerp(
-          Colors.lightGreen,
-          Colors.lime[600],
-          -(rssi + 45) / 10,
-        ),
-      );
-    } else if (rssi >= -65) {
-      return TextStyle(
-        color: Color.lerp(
-          Colors.lime[600],
-          Colors.amber,
-          -(rssi + 55) / 10,
-        ),
-      );
-    } else if (rssi >= -75) {
-      return TextStyle(
-        color: Color.lerp(
-          Colors.amber,
-          Colors.deepOrangeAccent,
-          -(rssi + 65) / 10,
-        ),
-      );
-    } else if (rssi >= -85) {
-      return TextStyle(
-        color: Color.lerp(
-          Colors.deepOrangeAccent,
-          Colors.redAccent,
-          -(rssi + 75) / 10,
-        ),
-      );
-    } else {
-      /*code symmetry*/
-      return const TextStyle(
-        color: Colors.redAccent,
-      );
-    }
-  }
-
   double mapRssiToScreenY(int rssi, BuildContext context) {
     // Assuming RSSI ranges from 0 to -75
     const double minRssi = -75;
